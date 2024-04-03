663bbd44ac57d0d3b8c5cbbfbdfd2fef3ecfd7b0
Hamaz Abdelghani <101653284+eljefe213@users.noreply.github.com>
2024-04-03 10:17:26 +0200

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,4 +1,6 @@
 <?php
-$example = file_get_contents('example.txt');
-echo nl2br($example);
+$example0 = file_get_contents('example.txt');
+echo nl2br($example0);
+$example1 = file("example.txt");
+echo $example1 [0];
 ?>
