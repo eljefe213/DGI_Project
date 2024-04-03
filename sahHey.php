aa9a2d35d1eab9127d67a7fc8371192e295ed003
Hamaz Abdelghani <101653284+eljefe213@users.noreply.github.com>
2024-04-03 10:23:39 +0200

    Update sahHey.php

--- sahHey.php
+++ sahHey.php
@@ -1,6 +1,9 @@
 <?php
 file_put_contents("test.txt", "This is a test! \r\n");
 echo "test.txt was created!";
-file_put_contents("test.txt","The test was successul! \r\n", FILE_APPEND);
+file_put_contents("test.txt","The test was successful! \r\n", FILE_APPEND);
+echo "test.txt was updated!";
+$example = file_get_contents("example.txt");
+file_put_contents("test.txt", $example, FILE_APPEND);
 echo "test.txt was updated!";
 ?> 
