ce05b7d284cb27e4ed6d3bf4865875dcdf2dba96
Hamaz Abdelghani <101653284+eljefe213@users.noreply.github.com>
2024-04-03 09:19:38 +0200

    Update page1.php

--- page1.php
+++ page1.php
@@ -1,10 +1,10 @@
-<form method="post" action="page2.php" >
-Please send the newsletter to: <br />
-Forename: <input type="text" name="forename" /><br />
-Surname: <input type="text" name="name" /><br />
-Email address: <input type="text" name="email" /><br />
-<input type="submit" value="Submit Form" />
-</form>
+// <form method="post" action="page2.php" >
+// Please send the newsletter to: <br />
+// Forename: <input type="text" name="forename" /><br />
+// Surname: <input type="text" name="name" /><br />
+// Email address: <input type="text" name="email" /><br />
+// <input type="submit" value="Submit Form" />
+// </form>
 
 <form action="page2.php" method="post">
 Please enter your password: <input type="password" name="password" />
