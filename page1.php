8026dfb2de91b011735f59e9484eefb0fb7a9a45
Hamaz Abdelghani <101653284+eljefe213@users.noreply.github.com>
2024-04-03 09:05:31 +0200

    Create page1.php

---
+++ page1.php
@@ -0,0 +1,7 @@
+<form method="post" action="page2.php" >
+Please send the newsletter to: <br />
+Forename: <input type="text" name="forename" /><br />
+Surname: <input type="text" name="name" /><br />
+Email address: <input type="text" name="email" /><br />
+<input type="submit" value="Submit Form" />
+</form>
