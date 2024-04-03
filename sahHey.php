af71a3b6352d6f5715b4c84c1f1735e6300eb140
Hamaz Abdelghani <101653284+eljefe213@users.noreply.github.com>
2024-04-03 10:22:03 +0200

    Update sahHey.php

--- sahHey.php
+++ sahHey.php
@@ -1,4 +1,6 @@
 <?php
 file_put_contents("test.txt", "This is a test! \r\n");
 echo "test.txt was created!";
+file_put_contents("test.txt","The test was successul! \r\n", FILE_APPEND);
+echo "test.txt was updated!";
 ?> 
